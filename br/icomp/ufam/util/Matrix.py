bd633953517518596c1a045ab9942807a7e7d83a
Romário Lira <rlb@icomp.ufam.edu.br>
2018-04-16 23:27:14 -0400

    3_commit
    Implementação das estruturas das matrizes de input e output da rede. Foram criados dois métodos de verificação de relação entre lugares e transições, um método para cada uma das duas matrizes.

--- br/icomp/ufam/util/Matrix.py
+++ br/icomp/ufam/util/Matrix.py
@@ -1,14 +1,86 @@
 class Matrix:
 
-    def __init__(self, PNet):
-        self.PNet = PNet
-        self.arcs = PNet.listA
-        self.linhas = len(PNet.listP)
-        self.colunas = len(PNet.listT)
-        self.matriz = []
+    def __init__(self, pnet):
+        """
 
-    """def setMatrix(self):
-        for i in range(1, self.linhas+1):
-            tmp = []
-            for j in range(1, self.colunas+1):
-                if"""
+        :type pnet: PNet
+        """
+        self.pnet = pnet
+        self.arcs = pnet.listA
+        self.linhas = len(pnet.listP)
+        self.colunas = len(pnet.listT)
+        self.matrizI = []
+        self.matrizO = []
+
+    def hasArcI(self, i, j):
+        """
+        Verifica relação de Input
+
+        Verifica se existe uma ligação de incidência de um lugar
+        para uma transição.
+
+        :param i: int
+        :param j: int
+        :return: true, false
+        """
+        place = self.pnet.listP[i]
+        transition = self.pnet.listT[j]
+        for arc in self.arcs:
+            if place.node.id in arc.source.node.id and transition.node.id \
+                    in arc.target.node.id:
+                return True
+        return False
+
+    def hasArcO(self, i, j):
+        """
+        Verifica relação de Output
+
+        Verifica se existe uma ligação de incidência de uma transição
+        para um lugar.
+
+        :param i: int
+        :param j: int
+        :return: true, false
+        """
+
+        place = self.pnet.listP[i]
+        transition = self.pnet.listT[j]
+        for arc in self.arcs:
+            if transition.node.id in arc.source.node.id and place.node.id \
+                    in arc.target.node.id:
+                return True
+        return False
+
+    def setMatrixI(self):
+        """
+        Matriz de Input
+
+        Cria a matriz de incidência, verifica se um lugar
+        tem relação com uma trasição.
+        :return: None
+        """
+        for i in range(0, self.linhas):
+            linha = []
+            for j in range(0, self.colunas):
+                if self.hasArcI(i, j) is True:
+                    linha.append(1)
+                else:
+                    linha.append(0)
+        self.matrizI.append(linha)
+
+    def setMatrixO(self):
+        """
+        Matriz de Output
+
+        Cria a matriz de saída, verifica se uma transição
+        tem relação com um lugar.
+        :return: None
+        """
+        for i in range(0, self.linhas):
+            linha = []
+            for j in range(0, self.colunas):
+                if self.hasArcO(i, j) is True:
+                    linha.append(1)
+                else:
+                    linha.append(0)
+        self.matrizI.append(linha)
